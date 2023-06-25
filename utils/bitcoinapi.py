5f8ab3ae952e975d88fe81bf2b51fa91d277179f
byiringiroscar <byiringoroscar@gmail.com>
2023-06-25 03:27:55 +0200

    Remove decouple for causiing errors

--- utils/bitcoinapi.py
+++ utils/bitcoinapi.py
@@ -1,10 +1,9 @@
 import requests
-from decouple import config
 
 async def get_bitcoin_trader():
     import requests
     url = 'https://rest.coinapi.io/v1/trades/latest'
-    headers = {'X-CoinAPI-Key' : config('BITCOIN_KEY')}
+    headers = {'X-CoinAPI-Key' : '3F69B3CB-B2C2-4B1A-B175-BBF449C1938D'}
     response = requests.get(url, headers=headers)
 
     return response.json()
